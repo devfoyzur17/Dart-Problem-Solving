a19f359dcfa77b405cd2fa21abe609c9f8094466
foyzur17 <foyzur17@gmail.com>
2022-05-29 21:43:12 +0600

    solve

---
+++ Day2/problem11.dart
@@ -0,0 +1,22 @@
+/*
+   
+  TODO Create a function that takes a string and returns a string with spaces in between all of the characters.
+  ? Examples:
+  spaceMeOut("space") ➞ "s p a c e" 
+*/
+
+void main(){
+
+ String result = spaceMeOut("space");
+ print(result);
+  
+}
+spaceMeOut(String value){
+  List temp =[];
+  for(var n in value.split("")){
+    temp.add(n+" ");
+  }
+  return temp.join().toString();
+  
+
+}
