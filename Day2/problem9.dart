a47689820dff0cdd60057007b264033db44d5e22
foyzur17 <foyzur17@gmail.com>
2022-05-29 20:51:59 +0600

    problem9 solved

---
+++ Day2/problem9.dart
@@ -0,0 +1,21 @@
+// TODO Create a function that takes a list of numbers and returns an inverted list.
+// ?Examples:
+//invertList([1, -2, 3, -4, 5]) ➞ [-1, 2, -3, 4, -5]
+
+// ignore_for_file: unused_local_variable
+
+void main() {
+  List list = [1, -2, 3, -4, 5];
+  List result = invertList(list);
+  print(result);
+}
+
+invertList(List list) {
+  List tempList = [];
+
+  list.forEach((element) {
+    tempList.add(element *= -1);
+  });
+
+  return tempList;
+}
