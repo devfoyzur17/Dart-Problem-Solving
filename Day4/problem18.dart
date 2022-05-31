587c8892f03900d4ba15a30848c34aec2b04aa81
foyzur17 <foyzur17@gmail.com>
2022-05-31 18:40:04 +0600

    problem solved

--- Day4/problem18.dart
+++ Day4/problem18.dart
@@ -3,8 +3,7 @@ TODO Create a function that returns a number, based on the string provided.
 TODO Here is a list of all digits:"one":  1"two":   2"three": 3"four":  4"five":  5"six":  6"seven":  7"eight": 8"nine":  9"zero":  0
 ?Examples:
 word("one") ➞ 1
-word
-("two") ➞ 2
+word("two") ➞ 2
 word("nine") ➞ 9
 Notes:!-All numbers will be single digit positive integers. 
 */
@@ -12,10 +11,13 @@ Notes:!-All numbers will be single digit positive integers.
  
 
 void main() {
-  print(word("zero"));
+  print(word("one"));
+  print(word("two"));
+  print(word("nine"));
+  print("Solved by Foyzur Rahaman");
 }
 
 word(String name) {
-  List list =["zero","one","two","three","fore","five"];
+  List list =["zero","one","two","three","fore","five","six","seven","eight","nine"];
   return  list.indexOf(name);  
 }
